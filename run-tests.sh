3e4428d162b1b22cfcbf7cb2ed3c8375a5441a1e
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2017-05-16 10:59:53 +0200

    tests: doctext execution simplification
    * doctests are now executed with pytest instead of sphinx. (closes #53)
    
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015, 2016, 2017 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -27,5 +27,4 @@ pydocstyle invenio_admin tests docs && \
 isort -rc -c -df && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+python setup.py test
