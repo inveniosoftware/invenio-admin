ee84a9a98d369b58c0dea2a7921ddb13004f2c8f
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-24 22:16:07 +0200

    chore(licenses): update license headers to use SPDX

--- invenio_admin/views.py
+++ invenio_admin/views.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
-# Copyright (C) 2021 Northwestern University.
-# Copyright (C) 2023 Graz University of Technology.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2015-2018 CERN.
+# SPDX-FileCopyrightText: 2021 Northwestern University.
+# SPDX-FileCopyrightText: 2023 Graz University of Technology.
+# SPDX-License-Identifier: MIT
 
 """Admin view class factory for creating protected admin views on-the-fly."""
 
